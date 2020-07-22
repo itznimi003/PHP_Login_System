57dbf10ce55f567a5175a4019da8ae6c5926959d
itznimi003 <68505106+itznimi003@users.noreply.github.com>
2020-07-22 16:04:43 +0530

    Update register.php

--- register.php
+++ register.php
@@ -101,7 +101,7 @@ mysqli_close($conn);
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-  <title>Occassion Saathi</title>
+  <title>PHP Login</title>
 </head>
 
 <body>
