1aa0df9909def55dab938e533b6b50b4abfb2d91
itznimi003 <68505106+itznimi003@users.noreply.github.com>
2020-07-22 16:02:49 +0530

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -25,29 +25,29 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Occassion Saathi</a>
+  <a class="navbar-brand" href="#">PHP Login</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
       <li class="nav-item active">
-          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <a class="nav-link" href="/PHP_Login_System/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="about.php">About</a>
+        <a class="nav-link" href="/PHP_Login_System/about.php">About</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="contact.php">Contact Us</a>
+        <a class="nav-link" href="/PHP_Login_System/contact.php">Contact Us</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="register.php">Register</a>
+        <a class="nav-link" href="/PHP_Login_System/register.php">Register</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
+        <a class="nav-link" href="/PHP_Login_System/login.php">Login</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="logout.php">Logout</a>
+        <a class="nav-link" href="/PHP_Login_System/logout.php">Logout</a>
       </li>
 
       
