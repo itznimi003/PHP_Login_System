dc9922d1d779b9efbb7060b1b7dfdb265558137b
itznimi003 <68505106+itznimi003@users.noreply.github.com>
2020-07-22 09:15:32 +0530

    Update register.php

--- register.php
+++ register.php
@@ -114,13 +114,13 @@ mysqli_close($conn);
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="/PHP_Login_System/register.php">Register</a>
+          <a class="nav-link" href="register.php">Register</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/PHP_Login_System/login.php">Login</a>
+          <a class="nav-link" href="login.php">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="/PHP_Login_System/logout.php">Logout</a>
+          <a class="nav-link" href="logout.php">Logout</a>
         </li>
 
 
