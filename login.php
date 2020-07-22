30abc407a82988829d5116d1d1a46c9947125279
itznimi003 <68505106+itznimi003@users.noreply.github.com>
2020-07-22 16:09:06 +0530

    Update login.php

--- login.php
+++ login.php
@@ -80,20 +80,20 @@ if(empty($err))
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">PHP Login System</a>
+  <a class="navbar-brand" href="#">PHP Login</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
   <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="register.php">Register</a>
+        <a class="nav-link" href="/PHP_Login_System/register.php">Register</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="login.php">Login</a>
+        <a class="nav-link" href="/PHP_Login_System/login.php">Login</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="logout.php">Logout</a>
+        <a class="nav-link" href="/PHP_Login_System/logout.php">Logout</a>
       </li>
 
       
