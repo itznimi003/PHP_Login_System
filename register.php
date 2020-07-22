a6874204df1dadd3459a76627210c915a97c9da5
itznimi003 <68505106+itznimi003@users.noreply.github.com>
2020-07-22 09:31:38 +0530

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
 <?php
-require_once "config.php";
+require_once "/PHP_Login_System/config.php";
 
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
@@ -106,7 +106,7 @@ mysqli_close($conn);
 
 <body>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">Occassion Saathi</a>
+    <a class="navbar-brand" href="#">PHP Login</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
       aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -114,13 +114,13 @@ mysqli_close($conn);
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="register.php">Register</a>
+          <a class="nav-link" href="/PHP_Login_System/register.php">Register</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="login.php">Login</a>
+          <a class="nav-link" href="/PHP_Login_System/login.php">Login</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="logout.php">Logout</a>
+          <a class="nav-link" href="/PHP_Login_System/logout.php">Logout</a>
         </li>
 
 
